186584226eba1b50a27635b00aeec9ac5da4d6cd
KavinduRasan <rasanjanakavindu60@gmail.com>
2024-06-09 22:28:56 +0530

    Add files via upload

---
+++ Steque_Implementation/src/Steque_Implementation/StequeUsingLinkedList.java
@@ -0,0 +1,90 @@
+package Steque_Implementation;
+
+
+	    public class StequeUsingLinkedList<T> {
+	        private Node<T> head;
+	        private Node<T> tail;
+	        private int size;
+
+	        private static class Node<T> {
+	            T data;
+	            Node<T> next;
+	            Node<T> prev;
+
+	            public Node(T data) {
+	                this.data = data;
+	                this.next = null;
+	                this.prev = null;
+	            }
+	        }
+
+	        public StequeUsingLinkedList() {
+	            head = null;
+	            tail = null;
+	            size = 0;
+	        }
+
+	        public void push(T element) {
+	            Node<T> newNode = new Node<>(element);
+	            if (isEmpty()) {
+	                head = newNode;
+	                tail = newNode;
+	            } else {
+	                newNode.next = head;
+	                head.prev = newNode;
+	                head = newNode;
+	            }
+	            size++;
+	        }
+
+	        public void enqueue(T element) {
+	            Node<T> newNode = new Node<>(element);
+	            if (isEmpty()) {
+	                head = newNode;
+	                tail = newNode;
+	            } else {
+	                tail.next = newNode;
+	                newNode.prev = tail;
+	                tail = newNode;
+	            }
+	            size++;
+	        }
+
+	        public T pop() {
+	            if (isEmpty()) {
+	                throw new IllegalStateException("Steque is empty. Cannot pop element.");
+	            }
+	            T data = head.data;
+	            head = head.next;
+	            if (head == null) {
+	                tail = null;
+	            } else {
+	                head.prev = null;
+	            }
+	            size--;
+	            return data;
+	        }
+
+	        public boolean isEmpty() {
+	            return size == 0;
+	        }
+
+	        public int size() {
+	            return size;
+	        }
+
+	        public static void main(String[] args) {
+	            StequeUsingLinkedList<Integer> steque = new StequeUsingLinkedList<>();
+	            steque.push(10);
+	            steque.push(20);
+	            steque.enqueue(30);
+
+	            System.out.println("Popped element: " + steque.pop());
+	            System.out.println("Is the steque empty? " + steque.isEmpty());
+	            System.out.println("Size of the steque: " + steque.size());
+	        }
+	    
+	}
+
+
+
---
+++ Steque_Implementation/src/Steque_Implementation/StequeUsingTwoStacks.java
@@ -0,0 +1,83 @@
+package Steque_Implementation;
+
+
+public class StequeUsingTwoStacks<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public StequeUsingTwoStacks() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void push(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void enqueue(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Steque is empty. Cannot pop element.");
+        }
+        T data = head.data;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        StequeUsingTwoStacks<Integer> steque = new StequeUsingTwoStacks<>();
+        steque.push(10);
+        steque.push(30);
+        steque.enqueue(50);
+
+        System.out.println("Popped element: " + steque.pop());
+        System.out.println("Is the steque empty? " + steque.isEmpty());
+        System.out.println("Size of the steque: " + steque.size());
+    }
+}
+
+
+
